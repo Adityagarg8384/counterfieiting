48576f60f8f71e25c0fb6e274006f439787b9a18
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-02-16 11:32:06 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,27 @@
 
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+task("accounts","Print the list of accounts", async (taskArgs, hre)=>{
+  const accounts= await hre.ethers.getSigners();
+
+  for(const account of accounts){
+    console.log(account.address);
+  }
+})
+
+// const Privatekey= process.env.NEXT_PUBLIC_PRIVATE_KEY;
+console.log(process.env.NEXT_PUBLIC_RPC_URL);
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  defaultNetwork:"sepolia",
+  networks:{
+    hardhat:{},
+    sepolia:{
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_ALCHEMEY_KEY}`,
+      accounts:[process.env.NEXT_PRIVATE_KEY],
+    }
+  }
+};
