9b3dcb582634d88317b6d00bf0894c06625956ba
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-02-16 11:32:51 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,1 +1,21 @@
 
+const hre= require("hardhat");
+require("dotenv").config();
+
+async function main(){
+    const Campaignfactory= await hre.ethers.getContractFactory("CampaignFactory");
+    const campaignfactory= await Campaignfactory.deploy();
+
+    await campaignfactory.deployed();
+
+    console.log("Factory deployed to :", campaignfactory.address);
+}
+
+main().then(()=>{
+    console.log("Process has started successfully");
+    process.exit(1);
+})
+.catch((err)=>{
+    console.log(err);
+    console.log("Some error occurred in the network");
+})
