499da17b7983874572e742a1773d6dfb22b2af5a
Aditya <adityagarg8384@gmail.com>
2025-10-19 15:58:58 +0530

    Adding files

--- frontend/lib/database.js
+++ frontend/lib/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const MONGO_URI= process.env.MONGO_URI;
 
 const dbconnect = async () => {
-  await mongoose.connect(NEXT_PUBLIC_MONGO_URI)
+  await mongoose.connect(MONGO_URI)
     .then(() => {
       console.log("Connection has been made successfully");
     })
